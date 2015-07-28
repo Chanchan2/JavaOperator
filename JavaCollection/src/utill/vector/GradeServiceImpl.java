e75a736a67f336fdd91dea15dd2f3e53828d47b2
평일반 <@PC30-PC>
2015-07-28 15:17:15 +0900

    2015.07.28 Vector 완성

--- JavaCollection/src/utill/vector/GradeServiceImpl.java
+++ JavaCollection/src/utill/vector/GradeServiceImpl.java
@@ -1,7 +1,9 @@
 package utill.vector;
-
+// 전체에서 특정 글자 바꾸기 단축키 CTRL + F
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Vector;
 /*
  프로그램을 코딩하다 보면 자료구조를 다루는 데 있어서 CRUD 액션의 반복을 경험하게 됩니다.
@@ -14,7 +16,7 @@ public class GradeServiceImpl implements GradeService{
 	// 필드에 아래 메소드들이 공유할 자료구조를 뭘 쓸까?
 	// 1. ArrayList	2. Vector	3. Stack	4.HashMap
 	// 순서 O, 중복 O,
-	private Vector<Grade> vec = new Vector<Grade>();
+	private List<Grade> vec = new ArrayList<Grade>();
 	Grade grade= new Grade(); // 디폴트 생성자가 있어서 에러 제거
 			
 	@Override
@@ -40,15 +42,15 @@ public class GradeServiceImpl implements GradeService{
 		// List 계열의 클래스 길이는 size()로 구한다.
 		for (int i = 0; i < vec.size(); i++) {
 			// 만약 vec가 배열이라면 vec.get(i)
-			String searchHakbun = vec.elementAt(i).getHakbun();
+			String searchHakbun = vec.get(i).getHakbun();
 			// 객체.메소드.메소드.메소드 이런 패턴이 가능한 때는 
 			// 반드시 return 값이 있는 메소드들 끼리 연결시에만 가능하다
 			// 이런 방식을 메소드 체인이라고 함
 			if (hakbun.equalsIgnoreCase(searchHakbun)) {
-				String name = vec.elementAt(i).getName();
-				int kor = vec.elementAt(i).getKor();
-				int eng = vec.elementAt(i).getEng();
-				int math = vec.elementAt(i).getMath();
+				String name = vec.get(i).getName();
+				int kor = vec.get(i).getKor();
+				int eng = vec.get(i).getEng();
+				int math = vec.get(i).getMath();
 				grade = new Grade(searchHakbun, name, kor, eng, math);
 				msg = grade.toString();
 				break;	// 중간이라도 학번이 일치하면 그대로 Stop 
@@ -64,13 +66,13 @@ public class GradeServiceImpl implements GradeService{
 		Vector<Grade> temp = new Vector<Grade>();
 		Grade grade = null;
 		for (int i = 0; i < vec.size(); i++) {
-			String searchName = vec.elementAt(i).getName();
+			String searchName = vec.get(i).getName();
 			// 고정값(파라미터).equals(변수값)
 			if (name.equalsIgnoreCase(searchName)) {
-				grade = new Grade(vec.elementAt(i).getHakbun(), searchName,
-						vec.elementAt(i).getKor(),
-						vec.elementAt(i).getEng(),
-						vec.elementAt(i).getMath());
+				grade = new Grade(vec.get(i).getHakbun(), searchName,
+						vec.get(i).getKor(),
+						vec.get(i).getEng(),
+						vec.get(i).getMath());
 				temp.add(grade);
 			} else {
 				// temp 라는 벡터를 완전히 비워서 null로 리턴
